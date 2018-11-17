9735fec96545680a5d183663185f4bf3d8062c81
john chau <john_chau@apple.com>
2018-11-16 19:01:31 -0800

    modified jTest.go

--- jTest.go
+++ jTest.go
@@ -6,33 +6,20 @@ import(
 
 func main(){
 
-	// var aParent organism = parent{name:"parent name", walk:"parent walk"}
 
 var john organism = child{name:"child name", parent: parent{name:"cool aid", walk:"cat walk"} }
 
+var tom organism = child{name:"tom name", parent: parent{name:"parent", walk:" the run"} }
 
+john = tom
 fmt.Println( john.getName() )
+
 fmt.Println( john.getWalk() )
 
+}
 
-
-/*
-for composition no polymorphism: cannot use setter method to change child  property or parent property. 
-can use dot operator to change child property or parent property.
-can  use getter method or dot operator to get child property or parent property.
-child inherit parent property and functions.
-
-
-for polymorphism composition, u inherit child property and function. cannot change parent or child property. cann use dot operator to get property.
-
-u cannot embed polymorphism.
-
-when polymorphism reassignment, you can use only the inherited property and function. cannot use override function
-
-when polymorphis, u cannot change property.
-*/
-
-
+type alien struct{
+	name string
 }
 
 type parent struct{
@@ -53,7 +40,7 @@ type organism interface{
 }
 
 
-/////////monkey struct
+/////////parent struct
 func(p parent) getName()string{
 	return "inherited from parent " + p.name
 }
@@ -64,16 +51,18 @@ func(p parent) getWalk()string{
 
 
 
-//////////////human struct
+//////////////child struct
 
-
-
-func( c child) getName()string{
-	return "override over parent "  + c.name
+func(c child) getName()string{
+	return "override parent " + c.name
 }
 
 func(c child) getWalk()string{
-	return "override over parent " + c.parent.walk
+	return "override parent " + c.parent.walk
 }
 
+/*
+inherited class should always override parent function. polymorphism or not
+
+*/
 
